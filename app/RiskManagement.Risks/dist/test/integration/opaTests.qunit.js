c938a2c5b4fc402fe85d1d53648443fd1404170f
Maximilian Harms <maximilian.harms@rb-omnishore.de>
2024-08-05 08:18:12 +0000

    sync

---
+++ app/RiskManagement.Risks/dist/test/integration/opaTests.qunit.js
@@ -0,0 +1,2 @@
+sap.ui.require(["sap/fe/test/JourneyRunner","RiskManagement/Risks/test/integration/FirstJourney","RiskManagement/Risks/test/integration/pages/RiskList","RiskManagement/Risks/test/integration/pages/RiskObjectPage"],function(e,s,i,t){"use strict";var e=new e({launchUrl:sap.ui.require.toUrl("RiskManagement/Risks")+"/index.html"});e.run({pages:{onTheRiskList:i,onTheRiskObjectPage:t}},s.run)});
+//# sourceMappingURL=opaTests.qunit.js.map
---
+++ app/RiskManagement.Risks/dist/test/integration/pages/RiskList.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/test/ListReport"],function(s){"use strict";var t={actions:{},assertions:{}};return new s({appId:"RiskManagement.Risks",componentId:"RiskList",contextPath:"/Risk"},t)});
+//# sourceMappingURL=RiskList.js.map
